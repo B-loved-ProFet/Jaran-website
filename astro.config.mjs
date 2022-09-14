ae0107b2991c2d08f5ff1ca43ada9872954b57b1
Szymi13 <urbanszymon13@gmail.com>
2022-09-14 18:25:48 +0200

    new reop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
-import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()]
+  integrations: [tailwind()]
 });

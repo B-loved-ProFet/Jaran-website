af3110687dc85a1ea96ac47bd7f621f8898f1b8c
Szymi13 <urbanszymon13@gmail.com>
2022-09-16 04:06:52 +0200

    navbar mobile menu todo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'RussianVelvet': '#1F0F53',
+			'NavajoWhite': '#FFDB99'
+		},
 		extend: {},
 	},
 	plugins: [],

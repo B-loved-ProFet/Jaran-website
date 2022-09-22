4dab59bae7a52e9f358390b69eb59e3deba61ab6
Szymi13 <urbanszymon13@gmail.com>
2022-09-22 22:41:11 +0200

    partytown and image optimalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import image from "@astrojs/image";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), partytown()],
   output: "server",
   adapter: vercel()
 });

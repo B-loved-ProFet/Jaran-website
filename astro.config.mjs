3b55a8535923f74096266cd25d75493de139e836
Szymi13 <urbanszymon13@gmail.com>
2022-10-07 19:08:00 +0200

    google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import image from "@astrojs/image";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), partytown({
+    //adds dataLayer.push as a forwarding event.
+    config:{
+      forward: ["dataLayer.push"],
+    }
+  })],
   output: "server",
   adapter: vercel()
 });
